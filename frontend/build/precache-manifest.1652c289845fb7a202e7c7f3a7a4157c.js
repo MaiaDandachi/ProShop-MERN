20e3cbfb20aae6b41bec018a240bb71643e9ccc7
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2020-10-21 13:49:05 +0300

    Prepare for deployment

---
+++ frontend/build/precache-manifest.1652c289845fb7a202e7c7f3a7a4157c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1b5070b5a5e9ef519ac09d59b87f6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc574e9f1b595c00171f",
+    "url": "/static/css/main.595afa7e.chunk.css"
+  },
+  {
+    "revision": "ce235fc3ba165e862b26",
+    "url": "/static/js/2.0ab67e27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0ab67e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc574e9f1b595c00171f",
+    "url": "/static/js/main.f9edd975.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
