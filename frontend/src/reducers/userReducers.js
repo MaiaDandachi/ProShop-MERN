32f6f8a7def0bd63e42823080ed44649dd26d26e
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2020-10-22 11:21:45 +0300

    make it better

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -24,6 +24,7 @@ import {
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAIL,
   USER_UPDATE_RESET,
+  USER_UPDATE_PROFILE_RESET,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -89,6 +90,9 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 
     case USER_UPDATE_PROFILE_FAIL:
       return { loading: false, error: action.payload };
+
+    case USER_UPDATE_PROFILE_RESET:
+      return {};
 
     default:
       return state;
